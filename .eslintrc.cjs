3c4b365e5e495ca7fe2a4b41af079f76e0ad16d5
Dominique Mattern <dominique@mattern.dev>
2023-11-01 11:21:01 +0100

    Add ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:astro/recommended',
+		'plugin:astro/jsx-a11y-recommended',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+		{
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro'],
+			},
+			rules: {
+				'astro/no-set-html-directive': 'error',
+				'astro/no-unused-css-selector': 'warn',
+			},
+		},
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {},
+};
