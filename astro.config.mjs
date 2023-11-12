b0a6e7330bd6065c9238f9ae8dffb88dd0297904
Dominique Mattern <dominique@mattern.dev>
2023-11-12 11:04:31 +0100

    Generate sitemap
    Closes #4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import mdx from "@astrojs/mdx";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+	site: 'https://mattern.dev',
+	integrations: [mdx(), sitemap()],
 });
