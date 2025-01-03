285dab0fae10f051a6984d4941d46e872f3290f7
Dominique Mattern <dominique@mattern.dev>
2025-01-03 15:09:43 +0100

    Format files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import icon from 'astro-icon';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mattern.dev',
+	site: "https://mattern.dev",
 	integrations: [mdx(), sitemap(), icon()],
 });
